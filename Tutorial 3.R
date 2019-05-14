c700d7e43a47e7578a8780dc3169b25d458e0218
Renard90 <al150879@alumnos.uacj.mx>
2019-05-14 16:20:56 -0600

    5th Commit

--- Tutorial 3.R
+++ Tutorial 3.R
@@ -71,3 +71,13 @@ gapminder_1952 <- gapminder %>%
 ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) +
   geom_point() +
   scale_x_log10()
+
+gapminder_1952 <- gapminder %>%
+  filter(year == 1952)
+
+# Scatter plot comparing pop and gdpPercap, with both axes on a log scale
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) +
+  geom_point() +
+  scale_x_log10() +
+  scale_y_log10()
+
