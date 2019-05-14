4e72a820236143c25ec9c7d2bd8fdb8b2f1f0fc8
Renard90 <al150879@alumnos.uacj.mx>
2019-05-14 16:01:09 -0600

    4th Commit

---
+++ Tutorial 3.R
@@ -0,0 +1,73 @@
+install.packages("tidyverse")
+install.packages("gapminder")
+library(tidyverse)
+library(gapminder)
+gapminder
+
+
+# Filter the gapminder dataset for the year 1957
+gapminder %>%
+  filter(year == 1957)
+
+# Filter for China in 2002
+gapminder %>%
+  filter(country == "China", year == 2002)
+
+gapminder %>%
+  filter(country == "China", year == 2002)
+
+# Sort in ascending order of lifeExp
+gapminder %>%
+  arrange(lifeExp)
+
+# Sort in descending order of lifeExp
+gapminder %>%
+  arrange(desc(lifeExp))
+
+#library(gapminder)
+#library(dplyr)
+
+# Filter for the year 1957, then arrange in descending order of population
+gapminder %>%
+  filter(year == 1957) %>%
+  arrange(desc(pop))
+
+# Use mutate to change lifeExp to be in months
+gapminder %>%
+  mutate(lifeExp = lifeExp * 12)
+
+# Use mutate to create a new column called lifeExpMonths
+gapminder %>%
+  mutate(lifeExpMonths = lifeExp * 12)
+
+# Filter, mutate, and arrange the gapminder dataset
+gapminder %>%
+  filter(year == 2007) %>%
+  mutate(lifeExpMonths = 12 * lifeExp) %>%
+  arrange(desc(lifeExpMonths))
+# Load the ggplot2 package as well
+
+# Create gapminder_1952
+gapminder_1952 <- gapminder %>%
+  filter(year == 1952)
+
+gapminder_1952
+
+# Change to put pop on the x-axis and gdpPercap on the y-axis
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) +
+  geom_point()
+
+gapminder_1952 <- gapminder %>%
+  filter(year == 1952)
+
+# Create a scatter plot with pop on the x-axis and lifeExp on the y-axis
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) +
+  geom_point()
+
+gapminder_1952 <- gapminder %>%
+  filter(year == 1952)
+
+# Change this plot to put the x-axis on a log scale
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) +
+  geom_point() +
+  scale_x_log10()
